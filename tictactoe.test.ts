30c55e7ba0a924f44730a5946160408933272284
JamesDoane <ijamesdoaneok@gmail.com>
2021-08-27 10:43:53 -0600

    Fully updated app and tests and such.

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -11,10 +11,50 @@ beforeAll(async () => {
 afterAll(async () => {
     await driver.quit()
 })
+describe("Test player actions" , () => {
+    test('I can start a game', async () => {
 
-test('I can start a game', async () => {
+        let button = await (await driver).findElement(By.id('start-game'));
+        await button.click();
+        
+    });
+    test('Can move into the top left square as a first move', async () => {
 
-    let button = await (await driver).findElement(By.id('start-game'));
-    await button.click();
+        let topLeft = await (await driver).findElement(By.id('cell-0'));
+        await topLeft.click();
+        await driver.sleep(3000)
+
+        expect(await topLeft.getText()).toBe('X')
+        
+    });
+    test('Can move into the top right square as a second move.', async () => {
+
+        let topRight = await (await driver).findElement(By.id('cell-2'));
+        await topRight.click();
+        await driver.sleep(3000)
+
+        expect(await topRight.getText()).toBe('X')
     
-});
+    });
+    test('Can move into the bottom right square as a third move', async () => {
+
+        let botRight = await (await driver).findElement(By.id('cell-8'));
+        await botRight.click();
+        await driver.sleep(3000)
+
+        expect(await botRight.getText()).toBe('X')
+    
+    });
+})
+describe("Computer Tests" , () =>{
+    test('Check that O is moving as expected.', async () => {
+
+        let topMid = await (await driver).findElement(By.id('cell-1'));
+        let midMid = await (await driver).findElement(By.id('cell-3'));
+
+        expect(await topMid.getText()).toBe('O')
+        expect(await midMid.getText()).toBe('O')
+        
+    });
+})
+
